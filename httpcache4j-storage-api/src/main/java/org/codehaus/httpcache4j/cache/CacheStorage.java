9f64243d9b81d5aef3c115a506b2ad3bf1b5fa67
hamnis <hamnis@aeef7db8-565a-0410-b616-bac81ff624f6>
2008-11-06 22:14:05 +0000

    - Added todos

--- httpcache4j-storage-api/src/main/java/org/codehaus/httpcache4j/cache/CacheStorage.java
+++ httpcache4j-storage-api/src/main/java/org/codehaus/httpcache4j/cache/CacheStorage.java
@@ -6,8 +6,11 @@ import org.codehaus.httpcache4j.HTTPRequest;
 import java.net.URI;
 
 /**
+ *
  * @author <a href="mailto:erlend@hamnaberg.net">Erlend Hamnaberg</a>
  */
+//TODO: Document.
+//TODO: Implement a version that is persistent.
 public interface CacheStorage {
 
     void put(URI requestURI, Vary vary, CacheItem cacheItem);
@@ -19,6 +22,7 @@ public interface CacheStorage {
     void clear();
 
     int size();
-
+    
+    //TODO: maybe not required any more...
     void invalidate(URI requestURI, CacheItem item);
 }
