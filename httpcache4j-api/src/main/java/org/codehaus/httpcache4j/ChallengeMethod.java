799c8fd3038756a9a6da4d63665851bf30625fd6
hamnis <hamnis@aeef7db8-565a-0410-b616-bac81ff624f6>
2008-11-09 14:00:52 +0000

    - Removed last remnants of dontation code.

--- httpcache4j-api/src/main/java/org/codehaus/httpcache4j/ChallengeMethod.java
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/ChallengeMethod.java
@@ -1,17 +1,7 @@
-/* 
- * $Header: $ 
- * 
- * Copyright (C) 2008 Escenic. 
- * All Rights Reserved.  No use, copying or distribution of this 
- * work may be made except in accordance with a valid license 
- * agreement from Escenic.  This notice must be 
- * included on all copies, modifications and derivatives of this 
- * work. 
- */
 package org.codehaus.httpcache4j;
 
 /**
- * @author <a href="mailto:erlend@escenic.com">Erlend Hamnaberg</a>
+ * @author <a href="mailto:erlend@hamnaberg.net">Erlend Hamnaberg</a>
  * @author last modified by $Author: $
  * @version $Id: $
  */
