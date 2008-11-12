3f8e3f8664f2ac10fdb01998e71ebc11475b61f3
hamnis <hamnis@aeef7db8-565a-0410-b616-bac81ff624f6>
2008-11-12 12:37:50 +0000

    - All files now have Copyright header.
    git-svn-id: file:///home/projects/httpcache4j/tmp/scm-svn-tmp/trunk@59 aeef7db8-565a-0410-b616-bac81ff624f6

--- httpcache4j-api/src/main/java/org/codehaus/httpcache4j/MIMEType.java
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/MIMEType.java
@@ -1,19 +1,17 @@
 /*
- * Copyright 2005-2007 Noelios Consulting.
+ * Copyright (c) 2008, The Codehaus. All Rights Reserved.
  *
- * The contents of this file are subject to the terms of the Common Development
- * and Distribution License (the "License"). You may not use this file except in
- * compliance with the License.
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * You can obtain a copy of the license at
- * http://www.opensource.org/licenses/cddl1.txt See the License for the specific
- * language governing permissions and limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *
- * When distributing Covered Code, include this CDDL HEADER in each file and
- * include the License file at http://www.opensource.org/licenses/cddl1.txt If
- * applicable, add the following below this CDDL HEADER, with the fields
- * enclosed by brackets "[]" replaced with your own identifying information:
- * Portions Copyright [yyyy] [name of copyright owner]
  */
 
 package org.codehaus.httpcache4j;
