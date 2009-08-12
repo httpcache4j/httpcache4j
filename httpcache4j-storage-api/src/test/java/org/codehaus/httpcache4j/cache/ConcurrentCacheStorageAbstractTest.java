cd389f2844e8401bd2a56f6e33dc3a617420c5b6
hamnis <hamnis@aeef7db8-565a-0410-b616-bac81ff624f6>
2009-08-12 12:42:39 +0000

    - Enforce that only one file is in the cache.
    git-svn-id: file:///home/projects/httpcache4j/tmp/scm-svn-tmp/trunk@247 aeef7db8-565a-0410-b616-bac81ff624f6

--- httpcache4j-storage-api/src/test/java/org/codehaus/httpcache4j/cache/ConcurrentCacheStorageAbstractTest.java
+++ httpcache4j-storage-api/src/test/java/org/codehaus/httpcache4j/cache/ConcurrentCacheStorageAbstractTest.java
@@ -52,7 +52,7 @@ public abstract class ConcurrentCacheStorageAbstractTest {
         Vary vary = new Vary();
         List<Callable<HTTPResponse>> calls = new ArrayList<Callable<HTTPResponse>>();
         for (int i = 0; i < numberOfIterations; i++) {
-            final HTTPResponse response = new HTTPResponse(new InputStreamPayload(new NullInputStream(40), MIMEType.APPLICATION_OCTET_STREAM), Status.OK, new Headers());
+            final HTTPResponse response = createCacheResponse();
             final URI uri = URI.create(String.valueOf(i));
             final Key key = new Key(uri, vary);
             Callable<HTTPResponse> call = new Callable<HTTPResponse>() {
@@ -60,6 +60,8 @@ public abstract class ConcurrentCacheStorageAbstractTest {
                     HTTPResponse cached = cacheStorage.insert(key, response);
                     CacheItem cacheItem = cacheStorage.get(new HTTPRequest(uri));
                     assertSame(cached, cacheItem.getResponse());
+                    cached = cacheStorage.insert(key, createCacheResponse());
+                    assertNotSame(cached, cacheItem.getResponse());
                     return cached;
                 }
             };
@@ -69,19 +71,29 @@ public abstract class ConcurrentCacheStorageAbstractTest {
         for (Future<HTTPResponse> response : responses) {
             try {
                 HTTPResponse real = response.get();
-                assertNotNull("Response was null", real);
+                assertResponse(real);
             } catch (ExecutionException e) {
                 fail(e.getCause().getMessage());
             }
         }
 
-
         assertEquals(expected, cacheStorage.size());
         assertEquals(expected, cacheStorage.size());
     }
 
+    private HTTPResponse createCacheResponse() {
+        return new HTTPResponse(new InputStreamPayload(new NullInputStream(40), MIMEType.APPLICATION_OCTET_STREAM), Status.OK, new Headers());
+    }
+
+    protected void assertResponse(final HTTPResponse response) {
+        assertNotNull("Response was null", response);
+        assertTrue("Payload was not here", response.hasPayload());
+        assertTrue("Payload was not available", response.getPayload().isAvailable());
+    }
+
     @After
     public void tearDown() {
+        cacheStorage.clear();
         service.shutdownNow();
     }
 }
