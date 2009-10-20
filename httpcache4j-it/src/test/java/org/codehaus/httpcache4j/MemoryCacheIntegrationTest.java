5bac0d3c1c23382a4ba5ac2a065f3005dc2eca33
hamnis <hamnis@aeef7db8-565a-0410-b616-bac81ff624f6>
2009-10-20 18:02:39 +0000

    - abstract integration test. - fixed artifact

---
+++ httpcache4j-it/src/test/java/org/codehaus/httpcache4j/MemoryCacheIntegrationTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2009. The Codehaus. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.codehaus.httpcache4j;
+
+import org.codehaus.httpcache4j.cache.CacheStorage;
+import org.junit.*;
+import org.codehaus.httpcache4j.cache.HTTPCache;
+import org.codehaus.httpcache4j.cache.MemoryCacheStorage;
+import org.codehaus.httpcache4j.resolver.HTTPClientResponseResolver;
+import org.codehaus.httpcache4j.payload.FilePayload;
+import org.codehaus.httpcache4j.util.TestUtil;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.net.URI;
+
+/**
+ * @author <a href="mailto:erlend@escenic.com">Erlend Hamnaberg</a>
+ * @version $Revision: $
+ */
+public class MemoryCacheIntegrationTest extends AbstractCacheIntegrationTest {
+
+    @Override
+    protected CacheStorage createStorage() {
+        return new MemoryCacheStorage();
+    }
+
+}
