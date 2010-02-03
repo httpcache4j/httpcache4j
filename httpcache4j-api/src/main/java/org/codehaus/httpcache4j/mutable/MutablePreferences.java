89757bad278c49c87a0aa536d4a6e1dce1ca3500
hamnis <hamnis@aeef7db8-565a-0410-b616-bac81ff624f6>
2010-02-03 23:29:11 +0000

    - Mutable request now useful

---
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/mutable/MutablePreferences.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2010. The Codehaus. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.codehaus.httpcache4j.mutable;
+
+import org.codehaus.httpcache4j.MIMEType;
+import org.codehaus.httpcache4j.preference.Preference;
+import org.codehaus.httpcache4j.preference.Preferences;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author <a href="mailto:hamnis@codehaus.org">Erlend Hamnaberg</a>
+ * @version $Revision: $
+ */
+public class MutablePreferences {
+    private Preferences preferences = new Preferences();
+
+    public void addLocale(Locale locale) {
+        preferences = preferences.addLocale(locale);
+    }
+
+    public void addLocale(Locale locale, double quality) {
+        preferences = preferences.addLocale(locale, quality);
+    }
+
+    public void addMIMEType(MIMEType mimeType, double quality) {
+        preferences = preferences.addMIMEType(mimeType, quality);
+    }
+
+    public void addMIMEType(MIMEType mimeType) {
+        preferences = preferences.addMIMEType(mimeType);
+    }
+
+    public void addCharset(String charset) {
+        preferences = preferences.addCharset(charset);
+    }
+
+    public void addCharset(String charset, double quality) {
+        preferences = preferences.addCharset(charset, quality);
+    }
+
+    public List<Preference<Locale>> getAcceptLocales() {
+        return preferences.getAcceptLocales();
+    }
+
+    public List<Preference<MIMEType>> getAcceptMIMETypes() {
+        return preferences.getAcceptMIMETypes();
+    }
+
+    public List<Preference<String>> getAcceptCharset() {
+        return preferences.getAcceptCharset();
+    }
+
+    public Preferences toPreferences() {
+        return preferences;
+    }
+}
--- httpcache4j-api/src/main/java/org/codehaus/httpcache4j/mutable/MutableRequest.java
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/mutable/MutableRequest.java
@@ -15,9 +15,95 @@
 
 package org.codehaus.httpcache4j.mutable;
 
+import org.codehaus.httpcache4j.*;
+import org.codehaus.httpcache4j.payload.Payload;
+
+import java.net.URI;
+
 /**
- * @author <a href="mailto:erlend@escenic.com">Erlend Hamnaberg</a>
+ * @author <a href="mailto:hamnis@codehaus.org">Erlend Hamnaberg</a>
  * @version $Revision: $
  */
 public class MutableRequest {
+    private final URI uri;
+    private final HTTPMethod method;
+    private final MutableHeaders headers = new MutableHeaders();
+    private final MutableConditionals conditionals = new MutableConditionals();
+    private final MutablePreferences preferences = new MutablePreferences();
+    private Challenge challenge;
+    private Payload payload;
+
+    public MutableRequest(URI uri) {
+        this(uri, HTTPMethod.GET);
+    }
+
+    public MutableRequest(URI uri, HTTPMethod method) {
+        this.uri = uri;
+        this.method = method;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public HTTPMethod getMethod() {
+        return method;
+    }
+
+    public MutableHeaders getHeaders() {
+        return headers;
+    }
+
+    public MutableConditionals getConditionals() {
+        return conditionals;
+    }
+
+    public MutablePreferences getPreferences() {
+        return preferences;
+    }
+
+    public Challenge getChallenge() {
+        return challenge;
+    }
+
+    public void setChallenge(Challenge challenge) {
+        this.challenge = challenge;
+    }
+
+    public Payload getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Payload payload) {
+        if (!method.canHavePayload()) {
+            throw new IllegalStateException(String.format("Unable to add payload to a %s request", method));
+        }
+        this.payload = payload;
+    }
+
+    public void addHeader(Header header) {
+        headers.add(header);
+    }
+
+    public void addHeader(String name, String value) {
+        addHeader(new Header(name, value));
+    }
+
+    public HTTPRequest toRequest() {
+        return new FromMutableRequest();
+    }
+
+    private class FromMutableRequest extends HTTPRequest {
+        public FromMutableRequest() {
+            super(uri,
+                  method,
+                  headers.toHeaders(),
+                  conditionals.toConditionals(),
+                  preferences.toPreferences(),
+                  challenge,
+                  payload
+            );
+        }
+    }
+
 }
