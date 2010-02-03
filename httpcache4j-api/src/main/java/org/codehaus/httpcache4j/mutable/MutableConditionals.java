bb0e444d940eb8c273f5803e1dc2889d748bd9a1
hamnis <hamnis@aeef7db8-565a-0410-b616-bac81ff624f6>
2010-02-03 23:29:11 +0000

    - Mutable request now useful
    git-svn-id: file:///home/projects/httpcache4j/tmp/scm-svn-tmp/trunk@343 aeef7db8-565a-0410-b616-bac81ff624f6

---
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/mutable/MutableConditionals.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2010. The Codehaus. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.codehaus.httpcache4j.mutable;
+
+import org.codehaus.httpcache4j.Conditionals;
+import org.codehaus.httpcache4j.Headers;
+import org.codehaus.httpcache4j.Tag;
+import org.joda.time.DateTime;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:hamnis@codehaus.org">Erlend Hamnaberg</a>
+ * @version $Revision: $
+ */
+public class MutableConditionals {
+    private Conditionals conditionals = new Conditionals();
+
+    public void addIfMatch(Tag tag) {
+        conditionals = conditionals.addIfMatch(tag);
+    }
+
+    public void addIfNoneMatch(Tag tag) {
+        conditionals = conditionals.addIfNoneMatch(tag);
+    }
+
+    public void ifModifiedSince(DateTime time) {
+        conditionals = conditionals.ifModifiedSince(time);
+    }
+
+    public void ifUnModifiedSince(DateTime time) {
+        conditionals = conditionals.ifUnModifiedSince(time);
+    }
+
+    public List<Tag> getMatch() {
+        return conditionals.getMatch();
+    }
+
+    public List<Tag> getNoneMatch() {
+        return conditionals.getNoneMatch();
+    }
+
+    public DateTime getModifiedSince() {
+        return conditionals.getModifiedSince();
+    }
+
+    public DateTime getUnModifiedSince() {
+        return conditionals.getUnModifiedSince();
+    }
+
+    public boolean isUnconditional() {
+        return conditionals.isUnconditional();
+    }
+
+    public Conditionals toConditionals() {
+        return conditionals;
+    }
+}
---
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/mutable/MutableHeaders.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2010. The Codehaus. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.codehaus.httpcache4j.mutable;
+
+import org.codehaus.httpcache4j.Header;
+import org.codehaus.httpcache4j.Headers;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:hamnis@codehaus.org">Erlend Hamnaberg</a>
+ * @version $Revision: $
+ */
+public class MutableHeaders {
+    private Headers headers = new Headers();
+
+    public void add(Header header) {
+        headers = headers.add(header);
+    }
+
+    public void add(String key, String value) {
+        headers = headers.add(key, value);
+    }
+
+    public void add(String name, List<Header> headers) {
+        this.headers = this.headers.add(name, headers);
+    }
+
+    public void add(Iterable<Header> headers) {
+        this.headers = this.headers.add(headers);
+    }
+
+    public List<Header> getHeaders(String headerKey) {
+        return headers.getHeaders(headerKey);
+    }
+
+    public Header getFirstHeader(String headerKey) {
+        return headers.getFirstHeader(headerKey);
+    }
+
+    public String getFirstHeaderValue(String headerKey) {
+        return headers.getFirstHeaderValue(headerKey);
+    }
+
+    public boolean contains(Header header) {
+        return headers.contains(header);
+    }
+
+    public boolean hasHeader(String headerName) {
+        return headers.hasHeader(headerName);
+    }
+
+    public int size() {
+        return headers.size();
+    }
+
+    public boolean isEmpty() {
+        return headers.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return headers.toString();
+    }
+
+    public Headers toHeaders() {
+        return headers;
+    }
+}
