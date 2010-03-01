420046d9da18a6cf1b7e73e01e579074c9b4b4fc
hamnis <hamnis@aeef7db8-565a-0410-b616-bac81ff624f6>
2010-03-01 11:28:41 +0000

    - URIBuilder is now fully tested with Encoded URI parts - URIEncoder and URIDecoder added (Wraps URLEncoder) - FomDataPayload uses the URIEncoder.

---
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/util/URIEncoder.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2010. The Codehaus. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.codehaus.httpcache4j.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.UnsupportedCharsetException;
+
+/**
+ * Wraps the {@link URLEncoder} of the JDK to provide a more useful interface
+ *
+ * @author <a href="mailto:hamnis@codehaus.org">Erlend Hamnaberg</a>
+ * @version $Revision: $
+ */
+public class URIEncoder {
+    public static String encodeUTF8(String input) {
+        return encode(input, "UTF-8");
+    }
+
+    public static String encode(String input, String encoding) {
+        try {
+            return URLEncoder.encode(input, encoding);
+        } catch (UnsupportedEncodingException e) {
+            throw new UnsupportedCharsetException(encoding);
+        }
+    }
+}
---
+++ httpcache4j-api/src/test/java/org/codehaus/httpcache4j/util/URIBuilderTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2010. The Codehaus. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.codehaus.httpcache4j.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URI;
+
+/**
+ * @author <a href="mailto:hamnis@codehaus.org">Erlend Hamnaberg</a>
+ * @version $Revision: $
+ */
+public class URIBuilderTest {
+    @Test
+    public void testConstructFromExistingURI() {
+        URI uri = URI.create("http://www.example.com/a/path/here");
+        URIBuilder.fromURI(uri);
+    }
+
+    @Test
+    public void testEqualityFromExistingURI() {
+        URI uri = URI.create("http://www.example.com/a/path/here");
+        URIBuilder builder = URIBuilder.fromURI(uri);
+        URI actual = builder.toURI();
+        Assert.assertEquals("URIs did not match",uri, actual);
+    }
+
+    @Test
+    public void testEqualityFromExistingURIWithQueryParameters() {
+        URI uri = URI.create("http://www.example.com/a/path/here?foo=bar&bar=foo&baz=foo&bazz=s");
+        URIBuilder builder = URIBuilder.fromURI(uri);
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+
+    @Test
+    public void testBuildPathFromBaseURI() {
+        URI base = URI.create("http://www.example.com");
+        URI uri = URI.create("http://www.example.com/a/path/here");
+        URIBuilder builder = URIBuilder.fromURI(base).path("a", "path", "here");
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+
+    @Test
+    public void testBuildToBaseURI() {
+        URI base = URI.create("http://www.example.com");
+        URIBuilder builder = URIBuilder.empty();
+        builder = builder.scheme("http").host("www.example.com");
+        Assert.assertEquals("URIs did not match", base, builder.toURI());
+    }
+
+    @Test
+    public void testBuildWithPathToURI() {
+        URI uri = URI.create("http://www.example.com/a/path/here");
+        URIBuilder builder = URIBuilder.empty();
+        builder = builder.scheme("http").host("www.example.com").path("a", "path", "here");
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+
+    @Test
+    public void testBuildWithQueryParameters() {
+        URI uri = URI.create("http://www.example.com/a/path/here?foo=bar&bar=foo");
+        URIBuilder builder = URIBuilder.empty();
+        builder = builder.scheme("http").host("www.example.com").path("a", "path", "here").addParameter("foo", "bar").addParameter("bar", "foo");
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+
+    @Test
+    public void testFromURIThenResetAndBuildWithQueryParameters() {
+        URI uri = URI.create("http://www.example.com/a/path/here?foo=bar&bar=foo");
+        URIBuilder builder = URIBuilder.fromURI(uri);
+        builder = builder.scheme("http").host("www.example.com").path("a", "path", "here").noParameters().addParameter("foo", "bar").addParameter("bar", "foo");
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+
+    @Test
+    public void testFromURIWithSingleParameter() {
+        URI uri = URI.create("http://www.example.com/a/path/here?foo=bar");
+        URIBuilder builder = URIBuilder.fromURI(uri);
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+
+    @Test
+    public void testBuildRelativeURI() {
+        URI uri = URI.create("/a/relative/PATH");
+        URIBuilder builder = URIBuilder.empty();
+        builder = builder.path("a", "relative", "PATH");
+        Assert.assertTrue(builder.isRelative());
+        Assert.assertEquals("URIs did not match", uri, builder.toAbsoluteURI());
+    }
+
+    @Test
+    public void testBuildRelativeURIWithMultiplePathInvocations() {
+        URI uri = URI.create("/PATH");
+        URIBuilder builder = URIBuilder.empty();
+        builder = builder.path("a").path("relative").path("PATH");
+        Assert.assertTrue(builder.isRelative());
+        Assert.assertEquals("URIs did not match", uri, builder.toAbsoluteURI());
+    }
+
+    @Test
+    public void testBuildRelativeURIWithoutStartingSlash() {
+        URI uri = URI.create("a/relative/PATH");
+        URIBuilder builder = URIBuilder.empty();
+        builder = builder.path("a", "relative", "PATH");
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+
+    @Test
+    public void testFromExistingURIWithEscapedChars() {
+        URI uri = URI.create("http://example.com/A+Testing+Escaped");
+        URIBuilder builder = URIBuilder.fromURI(uri);
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+        uri = URI.create("http://example.com/%2524+%2526+%253C+%253E+%253F+%253B+%2523+%253A+%253D+%252C+%2522+%2527+%257E+%252B");
+        builder = URIBuilder.fromURI(uri);
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+
+    @Test
+    public void testFromExistingURIWithEscapedQueryString() {
+        URI uri = URI.create("http://example.com?q=A+Testing+Escaped");
+        URIBuilder builder = URIBuilder.empty();
+        builder = builder.scheme("http").host("example.com").addParameter("q", "A+Testing+Escaped");
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+        uri = URI.create("http://example.com?q=%2524+%2526+%253C+%253E+%253F+%253B+%2523+%253A+%253D+%252C+%2522+%2527+%257E+%252B");
+        builder = URIBuilder.empty().scheme("http").host("example.com").noParameters().addParameter("q", URIEncoder.encodeUTF8("$ & < > ? ; # : = , \" ' ~ +"));
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+    @Test
+    public void testFromExistingURIWithEscapedQueryStringWithPlusSign() {
+        URI uri = URI.create("http://example.com?q=a%252Bb");
+        URIBuilder builder = URIBuilder.empty();
+        builder = builder.scheme("http").host("example.com").addParameter("q", URIEncoder.encodeUTF8("a+b"));
+        Assert.assertEquals("URIs did not match", uri, builder.toURI());
+    }
+}
