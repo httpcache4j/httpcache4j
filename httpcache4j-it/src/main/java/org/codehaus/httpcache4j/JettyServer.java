ebf057d33ff507ed8887a6897e57451621b2a781
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-16 10:42:47 +0600

    Remove codes that are now being used from external source
    In addition also remove dependencies no longer in use.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- httpcache4j-it/src/main/java/org/codehaus/httpcache4j/JettyServer.java
+++ httpcache4j-it/src/main/java/org/codehaus/httpcache4j/JettyServer.java
@@ -15,77 +15,10 @@
 
 package org.codehaus.httpcache4j;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.FilterMapping;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.escenic.http.servlet.*;
-
 /**
  * @author <a href="mailto:hamnis@codehaus.org">Erlend Hamnaberg</a>
  * @version $Revision: $
  */
 public class JettyServer {
     public static final int PORT = 10987;
-
-    private final Server server;
-
-    public JettyServer() {
-        this(PORT);
-    }
-
-    public JettyServer(int port) {
-        server = new Server(port);
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        addFilter(context, CacheControlFilter.class, "cc");
-        addFilter(context, ETagFilter.class, "etag");
-        addFilter(context, IfModifiedSinceFilter.class, "lm");
-        addFilter(context, BasicAuthFilter.class, "basic");
-        addFilter(context, DigestAuthFilter.class, "digest");
-        context.addServlet(RestServlet.class, "/*");
-
-        context.setContextPath("/");
-        server.setHandler(context);
-
-        server.setSendDateHeader(true);
-        server.setStopAtShutdown(true);
-    }
-
-    private void addFilter(ServletContextHandler handler, Class filter, String pathSpec) {
-        FilterHolder holder = new FilterHolder(filter);
-        if (pathSpec != null) {
-            holder.setInitParameter("path", pathSpec);
-        }
-        handler.addFilter(holder, "/*", FilterMapping.DEFAULT);
-    }
-
-    public void start() {
-        try {
-            server.start();
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void stop() {
-        try {
-            server.stop();
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void main(String[] args) {
-        JettyServer server = new JettyServer();
-        server.start();
-        try {
-            server.server.join();
-        } catch (InterruptedException e) {
-            server.stop();
-        }
-
-    }
-
 }
