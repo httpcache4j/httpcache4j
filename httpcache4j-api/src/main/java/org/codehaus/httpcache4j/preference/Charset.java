fcd98206f035513d2b43c84cecdfc17c5062cbf7
Erlend Hamnaberg <erlend@hamnaberg.net>
2013-11-14 09:51:50 +0100

    o set author tag

--- httpcache4j-api/src/main/java/org/codehaus/httpcache4j/preference/Charset.java
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/preference/Charset.java
@@ -1,7 +1,7 @@
 package org.codehaus.httpcache4j.preference;
 
 /**
- * @author Erlend Hamnaberg<erlend.hamnaberg@arktekk.no>
+ * @author Erlend Hamnaberg<hamnis@codehaus.org>
  */
 public class Charset {
     private final String name;
