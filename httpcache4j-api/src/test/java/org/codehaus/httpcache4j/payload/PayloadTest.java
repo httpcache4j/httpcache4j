83dc9e31ce65331e9fa197b794376d848304aea2
Erlend Hamnaberg <erlend@hamnaberg.net>
2015-02-12 21:12:36 +0100

    Binary test payload as well

--- httpcache4j-api/src/test/java/org/codehaus/httpcache4j/payload/PayloadTest.java
+++ httpcache4j-api/src/test/java/org/codehaus/httpcache4j/payload/PayloadTest.java
@@ -6,26 +6,41 @@ import org.codehaus.httpcache4j.MIMEType;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
 public class PayloadTest {
+    String s2 = "abcdefghijklmnopqrstuvwxyzæøå1234567890";
+    String s = String.format("abcdefghijklmnopqrstuvwxyz%s%s%s1234567890", (char)0x00E6, (char)0x00F8, (char)0x00E5);
 
     @Test
     public void generateByteArrayFromString() throws IOException {
-        String s = String.format("abcdefghijklmnopqrstuvwxyz%s%s%s1234567890", (char)0x00E6, (char)0x00F8, (char)0x00E5);
 
         ByteArrayPayload bytes = new ByteArrayPayload(s.getBytes(Charsets.UTF_8), MIMEType.APPLICATION_OCTET_STREAM);
         assertEquals(42L, bytes.length());
-        assertEquals(s, new String(ByteStreams.toByteArray(bytes.getInputStream()), Charsets.UTF_8));
+        String actual = new String(ByteStreams.toByteArray(bytes.getInputStream()), Charsets.UTF_8);
+        assertEquals(s, actual);
+        assertEquals(s2, actual);
     }
 
     @Test
     public void generateFromString() throws IOException {
-        String s = String.format("abcdefghijklmnopqrstuvwxyz%s%s%s1234567890", (char)0x00E6, (char)0x00F8, (char)0x00E5);
-
         StringPayload bytes = new StringPayload(s, MIMEType.APPLICATION_OCTET_STREAM);
         assertEquals(42L, bytes.length());
-        assertEquals(s, new String(ByteStreams.toByteArray(bytes.getInputStream()), Charsets.UTF_8));
+        String actual = new String(ByteStreams.toByteArray(bytes.getInputStream()), Charsets.UTF_8);
+        assertEquals(s, actual);
+        assertEquals(s2, actual);
+    }
+
+    @Test
+    public void binaryStream() throws IOException {
+        byte[] arr = new byte[2048];
+        Random rand = new Random();
+        rand.nextBytes(arr);
+        ByteArrayPayload p = new ByteArrayPayload(arr, MIMEType.APPLICATION_OCTET_STREAM);
+        byte[] actual = ByteStreams.toByteArray(p.getInputStream());
+        assertEquals(2048, p.length());
+        assertArrayEquals(arr, actual);
     }
 }
