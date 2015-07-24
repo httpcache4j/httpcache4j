daffddecf147af59907bec9406280921ee64b605
Erlend Hamnaberg <erlend@hamnaberg.net>
2015-07-24 18:52:55 +0100

    Reflectively get the status fields so we dont have to add it twice.
    Cleanup of alignments
    Make sure order of insertions are deterministic.

--- httpcache4j-api/src/main/java/org/codehaus/httpcache4j/Status.java
+++ httpcache4j-api/src/main/java/org/codehaus/httpcache4j/Status.java
@@ -17,8 +17,7 @@
 package org.codehaus.httpcache4j;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.lang.reflect.Field;
 
 /**
  * HTTP Status to return after handling a call.
@@ -26,58 +25,58 @@ import java.util.stream.Collectors;
  */
 public final class Status implements Comparable<Status> {
 
-    public static Status CONTINUE = new Status(100, "Continue");
-    public static Status SWITCHING_PROTOCOLS = new Status(101, "Switching Protocols");
-    public static Status OK = new Status(200, "OK");
-    public static Status CREATED = new Status(201, "Created");
-    public static Status ACCEPTED = new Status(202, "Accepted");
-    public static Status NON_AUTHORITATIVE_INFORMATION = new Status(203, "Non-Authoritative Information");
-    public static Status NO_CONTENT = new Status(204, "No Content");
-    public static Status RESET_CONTENT = new Status(205, "Reset Content");
-    public static Status PARTIAL_CONTENT = new Status(206, "Partial Content");
-    public static Status MULTIPLE_CHOICES = new Status(300, "Multiple Choices");
-    public static Status MOVED_PERMANENTLY = new Status(301, "Moved Permanently");
-    public static Status FOUND = new Status(302, "Found");
-    public static Status SEE_OTHER = new Status(303, "See Other");
-    public static Status NOT_MODIFIED = new Status(304, "Not Modified");
-    public static Status USE_PROXY = new Status(305, "Use Proxy");
-    public static Status TEMPORARY_REDIRECT = new Status(307, "Temporary Redirect");
-    public static Status BAD_REQUEST = new Status(400, "Bad Request");
-    public static Status UNAUTHORIZED = new Status(401, "Unauthorized");
-    public static Status PAYMENT_REQUIRED = new Status(402, "Payment Required"); //Reserved for future use!
-    public static Status FORBIDDEN = new Status(403, "Forbidden");
-    public static Status NOT_FOUND = new Status(404, "Not Found");
-    public static Status METHOD_NOT_ALLOWED = new Status(405, "Method Not Allowed");
-    public static Status NOT_ACCEPTABLE = new Status(406, "Not Acceptable");
-    public static Status PROXY_AUTHENTICATION_REQUIRED = new Status(407, "Proxy Authentication Required");
-    public static Status REQUEST_TIMEOUT = new Status(408, "Request Timeout");
-    public static Status CONFLICT = new Status(409, "Conflict");
-    public static Status GONE = new Status(410, "Gone");
-    public static Status LENGTH_REQUIRED = new Status(411, "Length Required");
-    public static Status PRECONDITION_FAILED = new Status(412, "Precondition Failed");
-    public static Status REQUEST_ENTITY_TOO_LARGE = new Status(413, "Request Entity Too Large");
-    public static Status REQUEST_URI_TOO_LONG = new Status(414, "Request-URI Too Long");
-    public static Status UNSUPPORTED_MEDIA_TYPE = new Status(415, "Unsupported Media Type");
+    public static Status CONTINUE                        = new Status(100, "Continue");
+    public static Status SWITCHING_PROTOCOLS             = new Status(101, "Switching Protocols");
+    public static Status OK                              = new Status(200, "OK");
+    public static Status CREATED                         = new Status(201, "Created");
+    public static Status ACCEPTED                        = new Status(202, "Accepted");
+    public static Status NON_AUTHORITATIVE_INFORMATION   = new Status(203, "Non-Authoritative Information");
+    public static Status NO_CONTENT                      = new Status(204, "No Content");
+    public static Status RESET_CONTENT                   = new Status(205, "Reset Content");
+    public static Status PARTIAL_CONTENT                 = new Status(206, "Partial Content");
+    public static Status MULTIPLE_CHOICES                = new Status(300, "Multiple Choices");
+    public static Status MOVED_PERMANENTLY               = new Status(301, "Moved Permanently");
+    public static Status FOUND                           = new Status(302, "Found");
+    public static Status SEE_OTHER                       = new Status(303, "See Other");
+    public static Status NOT_MODIFIED                    = new Status(304, "Not Modified");
+    public static Status USE_PROXY                       = new Status(305, "Use Proxy");
+    public static Status TEMPORARY_REDIRECT              = new Status(307, "Temporary Redirect");
+    public static Status BAD_REQUEST                     = new Status(400, "Bad Request");
+    public static Status UNAUTHORIZED                    = new Status(401, "Unauthorized");
+    public static Status PAYMENT_REQUIRED                = new Status(402, "Payment Required"); //Reserved for future use!
+    public static Status FORBIDDEN                       = new Status(403, "Forbidden");
+    public static Status NOT_FOUND                       = new Status(404, "Not Found");
+    public static Status METHOD_NOT_ALLOWED              = new Status(405, "Method Not Allowed");
+    public static Status NOT_ACCEPTABLE                  = new Status(406, "Not Acceptable");
+    public static Status PROXY_AUTHENTICATION_REQUIRED   = new Status(407, "Proxy Authentication Required");
+    public static Status REQUEST_TIMEOUT                 = new Status(408, "Request Timeout");
+    public static Status CONFLICT                        = new Status(409, "Conflict");
+    public static Status GONE                            = new Status(410, "Gone");
+    public static Status LENGTH_REQUIRED                 = new Status(411, "Length Required");
+    public static Status PRECONDITION_FAILED             = new Status(412, "Precondition Failed");
+    public static Status REQUEST_ENTITY_TOO_LARGE        = new Status(413, "Request Entity Too Large");
+    public static Status REQUEST_URI_TOO_LONG            = new Status(414, "Request-URI Too Long");
+    public static Status UNSUPPORTED_MEDIA_TYPE          = new Status(415, "Unsupported Media Type");
     public static Status REQUESTED_RANGE_NOT_SATISFIABLE = new Status(416, "Requested Range Not Satisfiable");
-    public static Status EXPECTATION_FAILED = new Status(417, "Expectation Failed");
-    public static Status INTERNAL_SERVER_ERROR = new Status(500, "Internal Server Error");
-    public static Status NOT_IMPLEMENTED = new Status(501, "Not Implemented");
-    public static Status BAD_GATEWAY = new Status(502, "Bad Gateway");
-    public static Status SERVICE_UNAVAILABLE = new Status(503, "Service Unavailable");
-    public static Status GATEWAY_TIMEOUT = new Status(504, "Gateway Timeout");
-    public static Status HTTP_VERSION_NOT_SUPPORTED = new Status(505, "HTTP Version Not Supported");
+    public static Status EXPECTATION_FAILED              = new Status(417, "Expectation Failed");
+    public static Status INTERNAL_SERVER_ERROR           = new Status(500, "Internal Server Error");
+    public static Status NOT_IMPLEMENTED                 = new Status(501, "Not Implemented");
+    public static Status BAD_GATEWAY                     = new Status(502, "Bad Gateway");
+    public static Status SERVICE_UNAVAILABLE             = new Status(503, "Service Unavailable");
+    public static Status GATEWAY_TIMEOUT                 = new Status(504, "Gateway Timeout");
+    public static Status HTTP_VERSION_NOT_SUPPORTED      = new Status(505, "HTTP Version Not Supported");
 
-    public static enum Category {
-        INFORMATIONAL(100, 199),
-        SUCCESS(200, 299),
-        REDIRECTION(300, 399),
-        CLIENT_ERROR(400, 499),
-        SERVER_ERROR(500, 599);
+    public enum Category {
+        INFORMATIONAL (100, 199),
+        SUCCESS       (200, 299),
+        REDIRECTION   (300, 399),
+        CLIENT_ERROR  (400, 499),
+        SERVER_ERROR  (500, 599);
 
         private final int min;
         private final int max;
 
-        private Category(int min, int max) {
+        Category(int min, int max) {
             this.min = min;
             this.max = max;
         }
@@ -96,55 +95,32 @@ public final class Status implements Comparable<Status> {
         }
     }
 
-    private static final Set<Status> STATUSES_WITHOUT_BODY = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(RESET_CONTENT, NO_CONTENT, NOT_MODIFIED)));
-    private static final Map<Integer, Status> STATUSES;
+    private static final Set<Status> STATUSES_WITHOUT_BODY = new HashSet<>(Arrays.asList(RESET_CONTENT, NO_CONTENT, NOT_MODIFIED));
+    static final Map<Integer, Status> STATUSES = reflectAllStaticStatusFields();
 
-    static {
-        List<Status> statuses = Arrays.asList(
-                CONTINUE,
-                SWITCHING_PROTOCOLS,
-                OK,
-                CREATED,
-                ACCEPTED,
-                NON_AUTHORITATIVE_INFORMATION,
-                NO_CONTENT,
-                RESET_CONTENT,
-                PARTIAL_CONTENT,
-                MULTIPLE_CHOICES,
-                MOVED_PERMANENTLY,
-                FOUND,
-                SEE_OTHER,
-                NOT_MODIFIED,
-                USE_PROXY,
-                TEMPORARY_REDIRECT,
-                BAD_REQUEST,
-                UNAUTHORIZED,
-                PAYMENT_REQUIRED,
-                FORBIDDEN,
-                NOT_FOUND,
-                METHOD_NOT_ALLOWED,
-                NOT_ACCEPTABLE,
-                PROXY_AUTHENTICATION_REQUIRED,
-                REQUEST_TIMEOUT,
-                CONFLICT,
-                GONE,
-                LENGTH_REQUIRED,
-                PRECONDITION_FAILED,
-                REQUEST_ENTITY_TOO_LARGE,
-                REQUEST_URI_TOO_LONG,
-                UNSUPPORTED_MEDIA_TYPE,
-                REQUESTED_RANGE_NOT_SATISFIABLE,
-                EXPECTATION_FAILED,
-                INTERNAL_SERVER_ERROR,
-                NOT_IMPLEMENTED,
-                BAD_GATEWAY,
-                SERVICE_UNAVAILABLE,
-                GATEWAY_TIMEOUT,
-                HTTP_VERSION_NOT_SUPPORTED
-        );
-
-        Map<Integer, Status> collect = statuses.stream().collect(Collectors.toMap(Status::getCode, Function.<Status>identity()));
-        STATUSES = Collections.unmodifiableMap(collect);
+    private static Map<Integer, Status> reflectAllStaticStatusFields() {
+        Field[] fields = Status.class.getDeclaredFields();
+        List<Status> statuses = new ArrayList<>();
+        for (Field field : fields) {
+            if (java.lang.reflect.Modifier.isStatic(field.getModifiers()) && field.getType() == Status.class) {
+                boolean accessible = field.isAccessible();
+                field.setAccessible(true);
+                try {
+                    Status status = (Status) field.get(null);
+                    if (status != null) {
+                        statuses.add(status);
+                    }
+                    field.setAccessible(accessible);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Map<Integer, Status> map = new TreeMap<>(Integer::compare);
+        for (Status s: statuses) {
+            map.put(s.getCode(), s);
+        }
+        return Collections.unmodifiableMap(map);
     }
 
     private final int code;
@@ -180,9 +156,7 @@ public final class Status implements Comparable<Status> {
     }
 
     public int compareTo(Status o) {
-        if (code > o.code) return 1;
-        if (code == o.code) return 0;
-        return -1;
+        return Integer.compare(code, o.code);
     }
 
     @Override
--- httpcache4j-api/src/test/java/org/codehaus/httpcache4j/StatusTest.java
+++ httpcache4j-api/src/test/java/org/codehaus/httpcache4j/StatusTest.java
@@ -3,32 +3,45 @@ package org.codehaus.httpcache4j;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
  * @author Erlend Hamnaberg<erlend.hamnaberg@arktekk.no>
  */
 public class StatusTest {
     @Test
     public void checkStatusSuccessCategory() {
-        Assert.assertEquals(Status.Category.SUCCESS, Status.OK.getCategory());
-        Assert.assertEquals(Status.Category.SUCCESS, Status.NO_CONTENT.getCategory());
+        assertEquals(Status.Category.SUCCESS, Status.OK.getCategory());
+        assertEquals(Status.Category.SUCCESS, Status.NO_CONTENT.getCategory());
     }
 
     @Test
     public void checkStatusRedirectionCategory() {
-        Assert.assertEquals(Status.Category.REDIRECTION, Status.FOUND.getCategory());
+        assertEquals(Status.Category.REDIRECTION, Status.FOUND.getCategory());
     }
 
     @Test
     public void checkStatusClientErrorCategory() {
-        Assert.assertEquals(Status.Category.CLIENT_ERROR, Status.NOT_FOUND.getCategory());
-        Assert.assertTrue(Status.BAD_REQUEST.isClientError());
-        Assert.assertFalse(Status.BAD_REQUEST.isServerError());
+        assertEquals(Status.Category.CLIENT_ERROR, Status.NOT_FOUND.getCategory());
+        assertTrue(Status.BAD_REQUEST.isClientError());
+        assertFalse(Status.BAD_REQUEST.isServerError());
     }
 
     @Test
     public void checkStatusServerErrorCategory() {
-        Assert.assertEquals(Status.Category.SERVER_ERROR, Status.INTERNAL_SERVER_ERROR.getCategory());
-        Assert.assertTrue(Status.INTERNAL_SERVER_ERROR.isServerError());
-        Assert.assertFalse(Status.INTERNAL_SERVER_ERROR.isClientError());
+        assertEquals(Status.Category.SERVER_ERROR, Status.INTERNAL_SERVER_ERROR.getCategory());
+        assertTrue(Status.INTERNAL_SERVER_ERROR.isServerError());
+        assertFalse(Status.INTERNAL_SERVER_ERROR.isClientError());
+    }
+
+    @Test
+    public void checkStatusFromCodeIsTheSameAsCachedInMap() {
+        assertEquals(Status.FORBIDDEN, Status.valueOf(403));
+        assertSame(Status.FORBIDDEN, Status.valueOf(403));
+    }
+
+    @Test
+    public void checkThatAllStatusesAreInTheMap() {
+        assertEquals(40, Status.STATUSES.size());
     }
 }
