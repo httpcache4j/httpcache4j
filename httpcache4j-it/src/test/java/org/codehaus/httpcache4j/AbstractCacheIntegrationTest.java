b2cae2c41659e716fb069dab102e3e4dff797c4b
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-07-20 09:31:48 +0200

    remove println

--- httpcache4j-it/src/test/java/org/codehaus/httpcache4j/AbstractCacheIntegrationTest.java
+++ httpcache4j-it/src/test/java/org/codehaus/httpcache4j/AbstractCacheIntegrationTest.java
@@ -219,7 +219,6 @@ public abstract class AbstractCacheIntegrationTest {
     public void GETWithVaryForAccept() {
         HTTPRequest request = new HTTPRequest(baseCustomRequestURI, HTTPMethod.GET).addHeader(HeaderConstants.ACCEPT, "text/plain");
         HTTPResponse response = cache.execute(request);
-        System.out.println("request.getRequestURI() = " + request.getRequestURI());
         assertEquals(Status.OK, response.getStatus());
         assertTrue(MIMEType.valueOf("text/plain").includes(response.getPayload().get().getMimeType()));
         assertFalse(response.isCached());
