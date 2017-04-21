httpcache4j/httpcache4j

commit 4e75e081f4502ca798097f3ba521e129e5798fc3

Remove joda-time and Optional updates

httpcache4j-core/src/main/java/org/codehaus/httpcache4j/cache/HTTPCacheHelper.java
@@ -16,13 +16,11 @@
 package org.codehaus.httpcache4j.cache;
 
 import org.codehaus.httpcache4j.*;
-import org.joda.time.DateTime;
-
+import net.hamnaberg.funclite.Optional;
 import java.io.IOException;
 import java.net.SocketException;
+import java.time.LocalDateTime;
 import java.util.*;
-
-import static org.codehaus.httpcache4j.HeaderConstants.CACHE_CONTROL;
 
 /**
  * @author <a href="mailto:hamnis@codehaus.org">Erlend Hamnaberg</a>
@@ -36,7 +34,7 @@ class HTTPCacheHelper {
         // ref http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html#sec13.5.1 and
         // http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html#sec13.5.3
         // We are a transparent cache
-        Set<String> headers = new HashSet<String>();
+        Set<String> headers = new HashSet<>();
         headers.add("Connection");
         headers.add("Keep-Alive");
         headers.add("Proxy-Authenticate");
@@ -105,11 +103,11 @@ class HTTPCacheHelper {
     HTTPRequest prepareConditionalGETRequest(HTTPRequest request, HTTPResponse staleResponse) {
         Conditionals conditionals = request.getHeaders().getConditionals();
         if (request.getMethod() == HTTPMethod.GET && conditionals.toHeaders().isEmpty()) {
-            if (staleResponse.getETag() != null) {
-                conditionals = new Conditionals().addIfNoneMatch(staleResponse.getETag());
+            if (staleResponse.getHeaders().getETag().isSome()) {
+                conditionals = new Conditionals().addIfNoneMatch(staleResponse.getHeaders().getETag().get());
             }
-            else if (staleResponse.getLastModified() != null) {
-                conditionals = conditionals.ifModifiedSince(staleResponse.getLastModified());
+            else if (staleResponse.getHeaders().getLastModified().isSome()) {
+                conditionals = conditionals.ifModifiedSince(staleResponse.getHeaders().getLastModified().get());
             }
             return request.headers(request.getHeaders().withConditionals(conditionals));
         }
@@ -121,46 +119,40 @@ class HTTPCacheHelper {
     }
 
     boolean isEndToEndReloadRequest(HTTPRequest request) {
-        CacheControl cacheControl = request.getCacheControl();
-        return request.getMethod().isCacheable() && cacheControl != null && cacheControl.isNoCache();
+        Optional<CacheControl> cacheControl = request.getCacheControl();
+        return request.getMethod().isCacheable() && cacheControl.exists(CacheControl::isNoCache);
     }
 
     boolean isCacheableRequest(HTTPRequest request) {
         if (request.getMethod().isCacheable()) {
-            if (request.getHeaders().contains(CACHE_CONTROL)) {
-                CacheControl cc = request.getCacheControl();
-                //If the request tells us that we shouldn't cache the response, then we don't.
-                return !cc.isNoCache() || !cc.isNoStore();
+            Optional<CacheControl> cc = request.getCacheControl();
+            return cc.forall(c -> c.isNoCache() || !c.isNoStore());
+        }
+        return false;
+    }
+
+    boolean allowStale(CacheItem item, HTTPRequest req, LocalDateTime requestTime) {
+        Optional<CacheControl> control = req.getCacheControl();
+        return control.exists(cc -> {
+            int maxStale = cc.getMaxStale();
+            if (maxStale > -1) {
+                long ttl = item.getTTL();
+                long age = item.getAge(requestTime);
+                return (ttl - maxStale - age) < 0;
             }
             return true;
-        }
-        return false;
+        });
     }
 
-    boolean allowStale(CacheItem item, HTTPRequest req, DateTime requestTime) {
-        if (req.getHeaders().contains(CACHE_CONTROL)) {
-            CacheControl control = req.getCacheControl();
-            int maxStale = control.getMaxStale();
-            if (maxStale > -1) {
-                int ttl = item.getTTL();
-                int age = item.getAge(requestTime);
-                if ((ttl - maxStale - age) < 0) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    HTTPResponse rewriteStaleResponse(HTTPRequest request, HTTPResponse cachedResponse, int age) {
+    HTTPResponse rewriteStaleResponse(HTTPRequest request, HTTPResponse cachedResponse, long age) {
         return rewriteResponse(request, cachedResponse, true, age, age < 0);
     }
 
-    HTTPResponse rewriteResponse(HTTPRequest request, HTTPResponse cachedResponse, int age) {
+    HTTPResponse rewriteResponse(HTTPRequest request, HTTPResponse cachedResponse, long age) {
         return rewriteResponse(request, cachedResponse, false, age, age < 0);
     }
 
-    private HTTPResponse rewriteResponse(HTTPRequest request, HTTPResponse cachedResponse, boolean stale, int age, boolean hasBeenCached) {
+    private HTTPResponse rewriteResponse(HTTPRequest request, HTTPResponse cachedResponse, boolean stale, long age, boolean hasBeenCached) {
         HTTPResponse response = cachedResponse;
         Headers headers = cachedResponse.getHeaders();
         if (request.getMethod().isSafe()) {
@@ -183,15 +175,15 @@ class HTTPCacheHelper {
         }
         if (request.getMethod() == HTTPMethod.GET) {
             List<Tag> noneMatch = request.getHeaders().getConditionals().getNoneMatch();
-            Tag eTag = response.getETag();
-            if (eTag != null && !noneMatch.isEmpty() && !hasBeenCached) {
-                if (noneMatch.contains(eTag) || noneMatch.contains(Tag.ALL)) {
+            Optional<Tag> eTag = response.getHeaders().getETag();
+            if (eTag.isSome() && !noneMatch.isEmpty() && !hasBeenCached) {
+                if (noneMatch.contains(eTag.get()) || noneMatch.contains(Tag.ALL)) {
                     response = new HTTPResponse(null, Status.NOT_MODIFIED, headers);
                 }
             }
-            DateTime lastModified = response.getLastModified();
-            DateTime modifiedSince = request.getHeaders().getConditionals().getModifiedSince();
-            if (lastModified != null && modifiedSince != null && !hasBeenCached) {
+            Optional<LocalDateTime> lastModified = response.getHeaders().getLastModified();
+            Optional<LocalDateTime> modifiedSince = request.getHeaders().getConditionals().getModifiedSince();
+            if (lastModified.isSome() && modifiedSince != null && !hasBeenCached) {
                 if (lastModified.equals(modifiedSince)) {
                     response = new HTTPResponse(null, Status.NOT_MODIFIED, headers);
                 }
@@ -208,8 +200,8 @@ class HTTPCacheHelper {
 
 
     boolean shouldBeStored(HTTPResponse response) {
-        boolean hasValidator = response.getLastModified() != null || response.getETag() != null;
-        boolean hasExpiry = response.getExpires() != null || (response.getCacheControl() != null && response.getCacheControl().getMaxAge() > 0);
+        boolean hasValidator = response.getHeaders().getLastModified().isSome() || response.getHeaders().getETag().isSome();
+        boolean hasExpiry = response.getHeaders().getExpires().isSome() || (response.getHeaders().getCacheControl().exists(cc -> cc.getMaxAge() > 0));
         return hasExpiry || hasValidator;
     }
 }

httpcache4j-api/pom.xml
@@ -28,14 +28,6 @@
   <name>Java HTTP cache :: API</name>
 
   <dependencies>
-    <!--<dependency>
-      <groupId>com.google.guava</groupId>
-      <artifactId>guava</artifactId>
-    </dependency>-->
-    <dependency>
-      <groupId>joda-time</groupId>
-      <artifactId>joda-time</artifactId>
-    </dependency>
     <dependency>
       <groupId>org.codehaus.httpcache4j.uribuilder</groupId>
       <artifactId>uribuilder</artifactId>

httpcache4j-api/src/main/java/org/codehaus/httpcache4j/Conditionals.java
@@ -17,9 +17,9 @@
 package org.codehaus.httpcache4j;
 
 import net.hamnaberg.funclite.Preconditions;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
+import net.hamnaberg.funclite.Optional;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -60,19 +60,19 @@ import java.util.stream.Collectors;
 public final class Conditionals {
     private final List<Tag> match;
     private final List<Tag> noneMatch;
-    private final DateTime modifiedSince;
-    private final DateTime unModifiedSince;
+    private final Optional<LocalDateTime> modifiedSince;
+    private final Optional<LocalDateTime> unModifiedSince;
     private static final String ERROR_MESSAGE = "The combination of %s and %s is undefined by the HTTP specification";
 
     public Conditionals() {
-        this(empty(), empty(), null, null);
+        this(empty(), empty(), Optional.none(), Optional.none());
     }
 
     private static List<Tag> empty() {
         return Collections.emptyList();
     }
 
-    public Conditionals(List<Tag> match, List<Tag> noneMatch, DateTime modifiedSince, DateTime unModifiedSince) {
+    public Conditionals(List<Tag> match, List<Tag> noneMatch, Optional<LocalDateTime> modifiedSince, Optional<LocalDateTime> unModifiedSince) {
         this.match = match;
         this.noneMatch = noneMatch;
         this.modifiedSince = modifiedSince;
@@ -87,7 +87,7 @@ public final class Conditionals {
      * @return a new Conditionals object with the If-Match tag added.
      */
     public Conditionals addIfMatch(Tag tag) {
-        Preconditions.checkArgument(modifiedSince == null, String.format(ERROR_MESSAGE, HeaderConstants.IF_MATCH, HeaderConstants.IF_MODIFIED_SINCE));
+        Preconditions.checkArgument(modifiedSince.isNone(), String.format(ERROR_MESSAGE, HeaderConstants.IF_MATCH, HeaderConstants.IF_MODIFIED_SINCE));
         Preconditions.checkArgument(noneMatch.isEmpty(), String.format(ERROR_MESSAGE, HeaderConstants.IF_MATCH, HeaderConstants.IF_NONE_MATCH));
         List<Tag> match = new ArrayList<>(this.match);
 
@@ -105,7 +105,7 @@ public final class Conditionals {
         else {
             throw new IllegalArgumentException("Tag ALL already in the list");
         }
-        return new Conditionals(Collections.unmodifiableList(match), empty(), null, unModifiedSince);
+        return new Conditionals(Collections.unmodifiableList(match), empty(), Optional.none(), unModifiedSince);
     }
 
     /**
@@ -121,9 +121,9 @@ public final class Conditionals {
      * @return a new Conditionals object with the If-None-Match tag added.
      */
     public Conditionals addIfNoneMatch(Tag tag) {
-        Preconditions.checkArgument(unModifiedSince == null, String.format(ERROR_MESSAGE, HeaderConstants.IF_NONE_MATCH, HeaderConstants.IF_UNMODIFIED_SINCE));
+        Preconditions.checkArgument(unModifiedSince.isNone(), String.format(ERROR_MESSAGE, HeaderConstants.IF_NONE_MATCH, HeaderConstants.IF_UNMODIFIED_SINCE));
         Preconditions.checkArgument(match.isEmpty(), String.format(ERROR_MESSAGE, HeaderConstants.IF_NONE_MATCH, HeaderConstants.IF_MATCH));
-        List<Tag> noneMatch = new ArrayList<Tag>(this.noneMatch);
+        List<Tag> noneMatch = new ArrayList<>(this.noneMatch);
         if (tag == null) {
             tag = Tag.ALL;
         }
@@ -138,7 +138,7 @@ public final class Conditionals {
         else {
             throw new IllegalArgumentException("Tag ALL already in the list");
         }
-        return new Conditionals(empty(), Collections.unmodifiableList(noneMatch), modifiedSince, null);
+        return new Conditionals(empty(), Collections.unmodifiableList(noneMatch), modifiedSince, Optional.none());
     }
 
     /**
@@ -153,12 +153,11 @@ public final class Conditionals {
      * @param time the time to check.
      * @return the conditionals with the If-Modified-Since date set.
      */
-    public Conditionals ifModifiedSince(DateTime time) {
+    public Conditionals ifModifiedSince(LocalDateTime time) {
         Preconditions.checkArgument(match.isEmpty(), String.format(ERROR_MESSAGE, HeaderConstants.IF_MODIFIED_SINCE, HeaderConstants.IF_MATCH));
-        Preconditions.checkArgument(unModifiedSince == null, String.format(ERROR_MESSAGE, HeaderConstants.IF_MODIFIED_SINCE, HeaderConstants.IF_UNMODIFIED_SINCE));
-        time = time.toDateTime(DateTimeZone.forID("UTC"));
-        time = time.withMillisOfSecond(0);
-        return new Conditionals(empty(), noneMatch, time, null);        
+        Preconditions.checkArgument(unModifiedSince.isNone(), String.format(ERROR_MESSAGE, HeaderConstants.IF_MODIFIED_SINCE, HeaderConstants.IF_UNMODIFIED_SINCE));
+        time = time.withNano(0);
+        return new Conditionals(empty(), noneMatch, Optional.some(time), Optional.none());
     }
 
     /**
@@ -173,12 +172,11 @@ public final class Conditionals {
      * @param time the time to check.
      * @return the conditionals with the If-Unmodified-Since date set.
      */
-    public Conditionals ifUnModifiedSince(DateTime time) {
+    public Conditionals ifUnModifiedSince(LocalDateTime time) {
         Preconditions.checkArgument(noneMatch.isEmpty(), String.format(ERROR_MESSAGE, HeaderConstants.IF_UNMODIFIED_SINCE, HeaderConstants.IF_NONE_MATCH));
-        Preconditions.checkArgument(modifiedSince == null, String.format(ERROR_MESSAGE, HeaderConstants.IF_UNMODIFIED_SINCE, HeaderConstants.IF_MODIFIED_SINCE));
-        time = time.toDateTime(DateTimeZone.forID("UTC"));
-        time = time.withMillisOfSecond(0);
-        return new Conditionals(match, empty(), null, time);
+        Preconditions.checkArgument(modifiedSince.isNone(), String.format(ERROR_MESSAGE, HeaderConstants.IF_UNMODIFIED_SINCE, HeaderConstants.IF_MODIFIED_SINCE));
+        time = time.withNano(0);
+        return new Conditionals(match, empty(), Optional.none(), Optional.some(time));
     }
 
     public List<Tag> getMatch() {
@@ -189,11 +187,11 @@ public final class Conditionals {
         return Collections.unmodifiableList(noneMatch);
     }
 
-    public DateTime getModifiedSince() {
+    public Optional<LocalDateTime> getModifiedSince() {
         return modifiedSince;
     }
 
-    public DateTime getUnModifiedSince() {
+    public Optional<LocalDateTime> getUnModifiedSince() {
         return unModifiedSince;
     }
 
@@ -202,7 +200,7 @@ public final class Conditionals {
      * @return {@code true} if the Conditionals represents a unconditional request. 
      */
     public boolean isUnconditional() {
-      return noneMatch.contains(Tag.ALL) || match.contains(Tag.ALL) || (match.isEmpty() && unModifiedSince == null) || (noneMatch.isEmpty() && modifiedSince == null) ;
+      return noneMatch.contains(Tag.ALL) || match.contains(Tag.ALL) || (match.isEmpty() && unModifiedSince.isNone()) || (noneMatch.isEmpty() && modifiedSince.isNone()) ;
     }
 
     /**
@@ -216,21 +214,21 @@ public final class Conditionals {
         if (!getNoneMatch().isEmpty()) {
             headers = headers.add(new Header(HeaderConstants.IF_NONE_MATCH, buildTagHeaderValue(getNoneMatch())));
         }
-        if (modifiedSince != null) {
-            headers = headers.set(HeaderUtils.toHttpDate(HeaderConstants.IF_MODIFIED_SINCE, modifiedSince));
+        if (modifiedSince.isSome()) {
+            headers = headers.set(HeaderUtils.toHttpDate(HeaderConstants.IF_MODIFIED_SINCE, modifiedSince.get()));
         }
-        if (unModifiedSince != null) {
-            headers = headers.set(HeaderUtils.toHttpDate(HeaderConstants.IF_UNMODIFIED_SINCE, unModifiedSince));
+        if (unModifiedSince.isSome()) {
+            headers = headers.set(HeaderUtils.toHttpDate(HeaderConstants.IF_UNMODIFIED_SINCE, unModifiedSince.get()));
         }
 
         return headers;
     }
 
     public static Conditionals valueOf(Headers headers) {
-        List<Tag> ifMatch = makeTags(headers.getFirstHeaderValue(HeaderConstants.IF_MATCH));
-        List<Tag> ifNoneMatch = makeTags(headers.getFirstHeaderValue(HeaderConstants.IF_NONE_MATCH));
-        DateTime modifiedSince = HeaderUtils.fromHttpDate(headers.getFirstHeader(HeaderConstants.IF_MODIFIED_SINCE));
-        DateTime unModifiedSince = HeaderUtils.fromHttpDate(headers.getFirstHeader(HeaderConstants.IF_UNMODIFIED_SINCE));
+        List<Tag> ifMatch = makeTags(headers.getFirstHeaderValue(HeaderConstants.IF_MATCH).orNull());
+        List<Tag> ifNoneMatch = makeTags(headers.getFirstHeaderValue(HeaderConstants.IF_NONE_MATCH).orNull());
+        Optional<LocalDateTime> modifiedSince = headers.getFirstHeader(HeaderConstants.IF_MODIFIED_SINCE).flatMap(HeaderUtils::fromHttpDate);
+        Optional<LocalDateTime> unModifiedSince = headers.getFirstHeader(HeaderConstants.IF_UNMODIFIED_SINCE).flatMap(HeaderUtils::fromHttpDate);
         return new Conditionals(ifMatch, ifNoneMatch, modifiedSince, unModifiedSince);
     }
 
@@ -241,7 +239,7 @@ public final class Conditionals {
         return Collections.unmodifiableList(Arrays.asList(ifMatch.split(",")).stream().
                 filter(m -> !Objects.toString(m, "").isEmpty()).
                 map(String::trim).
-                map(Tag::parse).collect(Collectors.toList()));
+                flatMap(t -> Tag.parse(t).stream()).collect(Collectors.toList()));
     }
 
     private String buildTagHeaderValue(List<Tag> match) {

httpcache4j-api/src/main/java/org/codehaus/httpcache4j/HTTPRequest.java
@@ -16,11 +16,12 @@
 
 package org.codehaus.httpcache4j;
 
+import net.hamnaberg.funclite.Optional;
 import org.codehaus.httpcache4j.payload.Payload;
 import org.codehaus.httpcache4j.uri.URIBuilder;
-import org.joda.time.DateTime;
 
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 /**
@@ -157,11 +158,8 @@ public final class HTTPRequest {
         return payload != null;
     }
 
-    public CacheControl getCacheControl() {
-        if (headers.contains(HeaderConstants.CACHE_CONTROL)) {
-            return new CacheControl(headers.getFirstHeader(HeaderConstants.CACHE_CONTROL));
-        }
-        return null;
+    public Optional<CacheControl> getCacheControl() {
+        return headers.getCacheControl();
     }
 
     public HTTPRequest cacheControl(CacheControl cc) {
@@ -176,11 +174,11 @@ public final class HTTPRequest {
         return headers(headers.withConditionals(headers.getConditionals().addIfMatch(tag)));
     }
 
-    public HTTPRequest withIfUnModifiedSince(DateTime dt) {
+    public HTTPRequest withIfUnModifiedSince(LocalDateTime dt) {
         return headers(headers.withConditionals(headers.getConditionals().ifUnModifiedSince(dt)));
     }
 
-    public HTTPRequest withIfModifiedSince(DateTime dt) {
+    public HTTPRequest withIfModifiedSince(LocalDateTime dt) {
         return headers(headers.withConditionals(headers.getConditionals().ifModifiedSince(dt)));
     }
 

httpcache4j-api/src/main/java/org/codehaus/httpcache4j/HTTPResponse.java
@@ -22,15 +22,12 @@ import org.codehaus.httpcache4j.annotation.Internal;
 import org.codehaus.httpcache4j.payload.InputStreamPayload;
 import org.codehaus.httpcache4j.payload.Payload;
 import org.codehaus.httpcache4j.util.IOUtils;
-import org.joda.time.DateTime;
 
 import java.io.InputStream;
-import java.net.URI;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Function;
 
-import static org.codehaus.httpcache4j.HeaderConstants.*;
+import static org.codehaus.httpcache4j.HeaderConstants.X_CACHE;
 
 /**
  * Represents a HTTP response delivered by the cache.
@@ -44,19 +41,7 @@ public final class HTTPResponse {
     private final StatusLine statusLine;
     private final Payload payload;
     private final Headers headers;
-
-    /**
-     * Consider removing the cached values from here...
-     */
-    private final DateTime date;
-    private final DateTime expires;
-    private final DateTime lastModified;
-    private final Tag etag;
-    private final Set<HTTPMethod> allowedMethods;
-    private final CacheControl cacheControl;
     private final boolean cached;
-    private final URI location;
-    private final URI contentLocation;
 
     public HTTPResponse(Payload payload, Status status, Headers headers) {
         this(payload, new StatusLine(status), headers);
@@ -66,15 +51,6 @@ public final class HTTPResponse {
         this.statusLine = Preconditions.checkNotNull(statusLine, "You must supply a Status");
         this.payload = payload;
         this.headers = Preconditions.checkNotNull(headers, "You must supply some Headers");
-
-        etag = headers.getETag();
-        lastModified = headers.getLastModified();
-        allowedMethods = headers.getAllow();
-        cacheControl = headers.getCacheControl();
-        date = headers.getDate();
-        expires = headers.getExpires();
-        location = headers.getLocation();
-        contentLocation = headers.getContentLocation();
 
         if (headers.contains(X_CACHE)) {
             Header cacheHeader = CacheHeaderBuilder.getBuilder().createHITXCacheHeader();
@@ -115,39 +91,9 @@ public final class HTTPResponse {
         return headers;
     }
 
-    public Tag getETag() {
-        return etag;
-    }
-
-    public DateTime getDate() {
-        return date;
-    }
-
-    public DateTime getExpires() {
-        return expires;
-    }
-
-    public DateTime getLastModified() {
-        return lastModified;
-    }
-
-    public CacheControl getCacheControl() {
-        return cacheControl;
-    }
-
-    public URI getLocation() {
-        return location;
-    }
-
-    public URI getContentLocation() {
-        return contentLocation;
-    }
-
     public boolean isCached() {
         return cached;
     }
-
-    public Set<HTTPMethod> getAllowedMethods() { return allowedMethods; }
 
     public <A> Optional<A> transform(final Function<Payload, A> f) {
         if (hasPayload()) {

httpcache4j-api/src/main/java/org/codehaus/httpcache4j/HeaderUtils.java
@@ -19,14 +19,14 @@ package org.codehaus.httpcache4j;
 import static org.codehaus.httpcache4j.HeaderConstants.*;
 
 import net.hamnaberg.funclite.Preconditions;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import net.hamnaberg.funclite.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -45,48 +45,47 @@ public final class HeaderUtils {
     private HeaderUtils() {
     }
 
-    public static DateTime fromHttpDate(Header header) {
+    public static Optional<LocalDateTime> fromHttpDate(Header header) {
         if (header == null) {
             return null;
         }
         if ("0".equals(header.getValue().trim())) {
-            return new DateTime(1970, 1, 1, 0, 0, 0, 0).withZone(DateTimeZone.forID("UTC"));
+            return Optional.some(LocalDateTime.of(LocalDate.of(1970, 1, 1), LocalTime.of(0, 0, 0, 0)));
         }
         return parseGMTString(header.getValue());
     }
 
-    public static DateTime parseGMTString(String value) {
+    public static Optional<LocalDateTime> parseGMTString(String value) {
         DateTimeFormatter formatter = getFormatter();
-        DateTime formattedDate = null;
         try {
-            formattedDate = formatter.parseDateTime(value);
-        } catch (IllegalArgumentException ignore) {
+            return Optional.some(LocalDateTime.from(formatter.parse(value)));
+        } catch (DateTimeParseException ignore) {
         }
 
-        return formattedDate;
+        return Optional.none();
     }
 
 
-    public static Header toHttpDate(String headerName, DateTime time) {
+    public static Header toHttpDate(String headerName, LocalDateTime time) {
         return new Header(headerName, toGMTString(time));
     }
 
-    public static String toGMTString(DateTime time) {
+    public static String toGMTString(LocalDateTime time) {
         DateTimeFormatter formatter = getFormatter();
-        return formatter.print(time);
+        return formatter.format(time);
     }
 
     private static DateTimeFormatter getFormatter() {
-        return DateTimeFormat.forPattern(PATTERN_RFC1123).
-                withZone(DateTimeZone.forID("UTC")).
+        return DateTimeFormatter.ofPattern(PATTERN_RFC1123).
+                withZone(ZoneId.of("UTC")).
                 withLocale(Locale.US);
     }
 
     public static long getHeaderAsDate(Header header) {
         try {
-            DateTime dateTime = fromHttpDate(header);
-            if (dateTime != null) {
-                return dateTime.getMillis();
+            Optional<LocalDateTime> dateTime = fromHttpDate(header);
+            if (dateTime.isSome()) {
+                return dateTime.get().toInstant(ZoneOffset.UTC).toEpochMilli();
             }
         }
         catch (Exception e) {
@@ -125,27 +124,24 @@ public final class HeaderUtils {
             return false;
         }
         if (headers.contains(CACHE_CONTROL)) {
-            final Header header = headers.getFirstHeader(CACHE_CONTROL);
-            CacheControl cc = new CacheControl(header);
-            if (cc.isNoCache() || cc.isNoStore()) {
+            Optional<CacheControl> cc = headers.getCacheControl();
+            if (cc.exists(cc2 -> cc2.isNoCache() || cc2.isNoStore())) {
                return false; 
             }
         }
         if (headers.contains(PRAGMA)) {
-            final Header header = headers.getFirstHeader(PRAGMA);
-            if (header.getValue().contains(NO_CACHE_HEADER_VALUE)) {
+            Optional<String> header = headers.getFirstHeaderValue(PRAGMA);
+            if (header.exists(s -> s.contains(NO_CACHE_HEADER_VALUE))) {
                 return false;
             }
         }
         if (headers.contains(EXPIRES)) {
-            Header expires = headers.getFirstHeader(EXPIRES);
-            DateTime expiresValue = HeaderUtils.fromHttpDate(expires);
-            Header date = headers.getFirstHeader(DATE);
-            if (expiresValue == null || date == null) {
+            Optional<LocalDateTime> expires = headers.getExpires();
+            Optional<LocalDateTime> date = headers.getDate();
+            if (expires.isNone() || date.isNone()) {
                 return false;
             }
-            DateTime dateValue = HeaderUtils.fromHttpDate(date);
-            if (expiresValue.isBefore(dateValue) || expiresValue.equals(dateValue)) {
+            if (expires.exists(e -> e.isBefore(date.get())) || expires.exists(e -> e.isEqual(date.get()))) {
                 return false;
             }
         }

commit ead23e965b6474d12c7ab6042b12d95d111a8df8

Correct isCacheable logic
The current logic seems to be broken since 4e75e081f4502ca798097f3ba521e129e5798fc3

`return !cc.isNoCache() || !cc.isNoStore();`
got replaced with 
`return cc.forall(c -> c.isNoCache() || !c.isNoStore());`

httpcache4j-core/src/main/java/org/codehaus/httpcache4j/cache/HTTPCacheHelper.java
@@ -129,7 +129,7 @@ class HTTPCacheHelper {
     boolean isCacheableRequest(HTTPRequest request) {
         if (request.getMethod().isCacheable()) {
             Optional<CacheControl> cc = request.getCacheControl();
-            return OptionalUtils.forall(cc, c -> c.isNoCache() || !c.isNoStore());
+            return OptionalUtils.forall(cc, c -> !c.isNoCache() || !c.isNoStore());
         }
         return false;
     }